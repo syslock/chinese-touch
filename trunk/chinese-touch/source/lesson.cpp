3cf186751808e925f8cd94e668defef895161d2a
syslock <syslock@fd0ee07c-f0f7-11de-b2c3-d77de7315633>
2010-02-27 18:45:50 +0000

    - added some documentation and a doxygen configuration file

--- trunk/chinese-touch/source/lesson.cpp
+++ trunk/chinese-touch/source/lesson.cpp
@@ -15,6 +15,7 @@
 #include "words_db.h"
 
 
+/*! Scans the directory structure "/chinese-touch/books/\<bookname\>/*" for book and lesson configuration files */
 void Library::rescan()
 {
     std::string books_path = "/" PROGRAM_NAME "/books";
@@ -40,6 +41,8 @@ void Library::rescan()
         if( S_ISDIR(book_stat.st_mode) && book_name!="." && book_name!=".." )
         {
             std::string lessons_path = books_path + "/" + book_name;
+			 /*! book configuration files are found once within each book directory 
+				and called \<bookdir\>.conf */
             std::string book_conf_path = lessons_path + "/" + book_name + ".conf";
             struct stat book_conf_stat;
             if( stat(book_conf_path.c_str(), &book_conf_stat)==-1 )
@@ -49,7 +52,7 @@ void Library::rescan()
                 continue;
             }
             Book* book = new Book( book_name, this );
-            book->parse_config( book_conf_path );
+            book->parse_config( book_conf_path ); //! \see Book::parse_config
 			book->id = WordsDB::get_book_id( *book, true );
             (*this)[ book_name ] = book;
             LOG( "book: " << book_path );
@@ -98,25 +101,32 @@ void Library::rescan()
 							lesson->id = WordsDB::get_lesson_id( *lesson, true );
 							(*book)[ lesson_number ] = lesson;
 						}
+						/*! The following lesson specific configuration files may appear multiple times 
+							within each book directory, once for each lesson: */
+						/*! - A lessons dictionary table is called \<lesson-number\>.dict */
 						if( lesson_extension == "dict" )
 						{
-							lesson->parse_dictionary( lesson_path, this->dictionary );
+							lesson->parse_dictionary( lesson_path, this->dictionary ); //! \see Lesson::parse_dictionary
 						}
+						/*! - A lessons base configuration file is called \<lesson-number\>.conf */
 						else if( lesson_extension == "conf" )
 						{
-							lesson->parse_config( lesson_path );
+							lesson->parse_config( lesson_path ); //! \see Lesson::parse_config
 						}
+						/*! - A lessons text file is called \<lesson-number\>.text */
 						else if( lesson_extension == "text" )
 						{
-							lesson->parse_text( lesson_path, lesson->lesson_texts );
+							lesson->parse_text( lesson_path, lesson->lesson_texts ); //! \see Lesson::parse_text
 						}
+						/*! - A lessons grammar text file is called \<lesson-number\>.grammar */
 						else if( lesson_extension == "grammar" )
 						{
-							lesson->parse_text( lesson_path, lesson->grammar_texts );
+							lesson->parse_text( lesson_path, lesson->grammar_texts ); //! \see Lesson::parse_text
 						}
+						/*! - A lessons excercises text file is called \<lesson-number\>.text */
 						else if( lesson_extension == "exercise" )
 						{
-							lesson->parse_text( lesson_path, lesson->exercises );
+							lesson->parse_text( lesson_path, lesson->exercises ); //! \see Lesson::parse_text
 						}
 					}
                 }
@@ -128,6 +138,7 @@ void Library::rescan()
 }
 
 
+/*! Parse a book configuration file, containing various key=value pairs (one per line), defining book properties */
 void Book::parse_config( const std::string& conf_file_name )
 {
 	std::ifstream book_conf_file( conf_file_name.c_str() );
@@ -142,27 +153,33 @@ void Book::parse_config( const std::string& conf_file_name )
 		{
 			key = line.substr( 0, equals_pos );
 			value = line.substr( equals_pos+1 );
+			//! \verbatim title=Title of the book \endverbatim
 			if( key=="title" )
 			{
 				this->title = value;
 			}
-			else if( key=="description" )
+			//! \verbatim description=Longer description of the book (one line) \endverbatim
+			else if( key=="description" ) 
 			{
 				this->description = value;
 			}
-			else if( key=="author" )
+			//! \verbatim author=Name of the auther of the book \endverbatim
+			else if( key=="author" ) 
 			{
 				this->author = value;
 			}
-			else if( key=="publisher" )
+			//! \verbatim publisher=Publisher of the book \endverbatim
+			else if( key=="publisher" ) 
 			{
 				this->publisher = value;
 			}
-			else if( key=="isbn" )
+			//! \verbatim isbn=<isbn> \endverbatim
+			else if( key=="isbn" ) 
 			{
 				this->isbn = value;
 			}
-			else if( key=="year" )
+			//! \verbatim year=<yearnum> \endverbatim
+			else if( key=="year" ) 
 			{
 				this->year = atoi( value.c_str() );
 			}
@@ -171,6 +188,7 @@ void Book::parse_config( const std::string& conf_file_name )
 }
 
 
+/*! Parse a lesson configuration file, containing various key=value pairs (one per line), defining lesson properties */
 void Lesson::parse_config( const std::string& conf_file_name )
 {
 	std::ifstream lesson_conf_file( conf_file_name.c_str() );
@@ -185,10 +203,12 @@ void Lesson::parse_config( const std::string& conf_file_name )
 		{
 			key = line.substr( 0, equals_pos );
 			value = line.substr( equals_pos+1 );
+			//! \verbatim title=Title of the lesson \endverbatim
 			if( key=="title" )
 			{
 				this->title = value;
 			}
+			//! \verbatim description=Longer description of purpose and contents of the lesson (one line) \endverbatim
 			else if( key=="description" )
 			{
 				this->description = value;
@@ -198,6 +218,25 @@ void Lesson::parse_config( const std::string& conf_file_name )
 }
 
 
+/*! Parse a dictionary file, containing a table definition in MediaWiki format. 
+	You can easily create those tables using OpenOffice and its MediaWiki export filter. 
+	Anyway here is an example of the expected plain text format: \n
+\verbatim
+{|  
+
+...
+
+|- 
+| 玩
+| wán
+| play (the game)
+| (n)
+| It can't be used like "play the piano" or "play video"... etc.
+
+...
+
+|}
+\endverbatim */
 void Lesson::parse_dictionary( const std::string& dict_file_name, Dictionary&  dictionary )
 {
 	struct stat dict_file_stats;
@@ -219,7 +258,7 @@ void Lesson::parse_dictionary( const std::string& dict_file_name, Dictionary&  d
     {
         dict_file.getline( line_buffer, sizeof(line_buffer) );
         std::string line = line_buffer;
-        if( line.substr(0, 2) == "|-" )
+        if( line.substr(0, 2) == "|-" ) //! "|-" At the beginning of a line marks the begin of a new word row.
         {
             if( word_count )
             {
@@ -241,7 +280,8 @@ void Lesson::parse_dictionary( const std::string& dict_file_name, Dictionary&  d
             word_count++;
             column = 0;
         }
-        else if( line.substr(0, 2) == "|}" && word_count )
+        else if( line.substr(0, 2) == "|}" && word_count ) /*! "|}" At the beginning of a line marks the 
+																end of the dictionary table. */
         {
             NewWord* word = new NewWord( hanzi, pinyin, this );
             word->definitions[ definition.lang ] = new Definition( definition );
@@ -249,7 +289,8 @@ void Lesson::parse_dictionary( const std::string& dict_file_name, Dictionary&  d
 			dictionary.add_new_word( word );
             column = 0;
         }
-        else if( line.substr(0, 2) == "| " )
+        else if( line.substr(0, 2) == "| " ) /*! "| " The pipe the beginning of a line, followed by a space and 
+												arbitrary text defines a column of a word row. */
         {
             column++;
             line = line.substr(2);
@@ -262,17 +303,25 @@ void Lesson::parse_dictionary( const std::string& dict_file_name, Dictionary&  d
 		}
         switch( column )
         {
-        case 1: hanzi.append( line ); break;
-        case 2: pinyin.append( line ); break;
-        case 3: definition.translation.append( line ); break;
-        case 4: definition.word_type.append( line ); break;
-        case 5: definition.comment.append( line ); break;
+		/*! A word rows colums have to appear after the word rows begin marker and 
+			before the next words begin marker and before the dictionary tables end marker.
+			The columns are expected to appear in the following order: */
+		case 1: hanzi.append( line ); break; //! - column 1: The foreign language word
+		case 2: pinyin.append( line ); break; //! - column 2: The foreign word pronunciation
+		case 3: definition.translation.append( line ); break; //! - column 3: The native language translation
+		case 4: definition.word_type.append( line ); break; //! - column 4: A short, usually abbreviated, string, defining the word type
+		case 5: definition.comment.append( line ); break; //! - column 5: An additional comment in native language
+		/*! Column order is mandatory, but the actual number of columns may differ from the  
+			number of meaningful columns described above, though the value of exceeding columns 
+			will be ignored. Column values may contain line breaks or being left blank. Pipes and curly 
+			brackets should be avoided within column values. */
         }
     }
     dict_file.close();
 }
 
 
+/*! Copy the contents of a lessons text file into an in memory buffer for later use */
 void Lesson::parse_text( const std::string& text_file_name, TextVector& container )
 {
 	std::ifstream text_file( text_file_name.c_str() );
