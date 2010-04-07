a6b2d1394a10c6dd8ac22fe4c14867cf915999af
syslock <syslock@fd0ee07c-f0f7-11de-b2c3-d77de7315633>
2010-04-07 22:21:15 +0000

    - added extra code to generate additional fulltext lookup tables for pronunciation, translation and comment fields

--- trunk/chinese-touch/tools/handedict_to_csv.py
+++ trunk/chinese-touch/tools/handedict_to_csv.py
@@ -39,10 +39,19 @@ second_mapping = {
 	"ü5" : "ü",
 }
 
+untone_mapping = {}
+for key in first_mapping:
+	if key[-1] != "5":
+		untone_mapping[ first_mapping[key] ] = key[0]
+for key in second_mapping:
+	if key[-1] != "5":
+		untone_mapping[ second_mapping[key] ] = key[0]
+
 def translate_pinyin( raw_pinyin ):
 	syllables = raw_pinyin.split(" ")
 	new_syllables = []
 	for _syl in syllables:
+		_syl = _syl.lower()
 		try:
 			syl, tone = _syl[:-1].decode("utf-8"), int(_syl[-1:])
 		except:
@@ -78,6 +87,48 @@ def translate_pinyin( raw_pinyin ):
 		#print _syl + " -> " + new_syl
 	return " ".join(new_syllables)
 
+
+extended_lower_map = {
+	"Ä" : "ä",
+	"Ö" : "ö",
+	"Ü" : "ü",
+	"Ā" : "ā",
+	"Á" : "á",
+	"Ǎ" : "ǎ",
+	"À" : "à",
+	"Ē" : "ē",
+	"É" : "é",
+	"Ě" : "ě",
+	"È" : "è",
+	"Ō" : "ō",
+	"Ó" : "ó",
+	"Ǒ" : "ǒ",
+	"Ò" : "ò",
+	"Ī" : "ī",
+	"Í" : "í",
+	"Ǐ" : "ǐ",
+	"Ì" : "ì",
+	"Ū" : "ū",
+	"Ú" : "ú",
+	"Ǔ" : "ǔ",
+	"Ù" : "ù",
+	"Ǖ" : "ǖ",
+	"Ǘ" : "ǘ",
+	"Ǚ" : "ǚ",
+	"Ǜ" : "ǜ",
+}
+
+def extended_lower( s ):
+	s = s.lower()
+	result = ""
+	for c in s.decode("utf-8"):
+		c = c.encode("utf-8")
+		if extended_lower_map.has_key(c):
+			c = extended_lower_map[c]
+		result += c
+	return result
+
+
 col_names = [ 
 	"atime",
 	"id",
@@ -91,6 +142,12 @@ col_names = [
 	"rating",
 	"file_id",
 	"file_offset"
+]
+
+ft_col_names = [
+	"pronunciation",
+	"definition",
+	"comment"
 ]
 
 if "--inserts" in sys.argv[1:]:
@@ -115,6 +172,10 @@ comment=""
 rating="0"
 file_id="0"
 file_offset=0
+
+# unique list fulltext patterns, referencing unique word id lists
+ft_patterns = {}
+
 for line in sys.stdin:
 	line = line.replace( "\t", " " );
 	line = line.replace( "(u.E.)", "" );
@@ -156,13 +217,22 @@ for line in sys.stdin:
 		comment += result[3]
 		if "--inserts" in sys.argv[1:]:
 			insert = "insert into words ("+",".join(col_names)+") values ("
+			ft_pattern_list = []
 			for i in xrange(len(col_names)):
 				if( i ): insert+=","
 				value = ("%("+col_names[i]+")s") % locals()
+				if( col_names[i] in ft_col_names ):
+					ft_pattern_list += re.split( "[ ,;:.\!\?\-/\(\)\[\]\{\}\<\>0-9'\"]", value )
 				value = value.replace( "'", "''" ).strip()
 				insert += "'"+value+"'"
 			insert += ");"
 			print insert
+			for ft_pattern in ft_pattern_list:
+				ft_pattern = extended_lower( ft_pattern )
+				if ft_pattern!="":
+					if ft_patterns.has_key(ft_pattern):
+						ft_patterns[ft_pattern][id] = True
+					else: ft_patterns[ft_pattern] = { id : True }
 		else:
 			for i in xrange(len(col_names)):
 				if( i ): sys.stdout.write( "\t" )
@@ -176,5 +246,20 @@ for line in sys.stdin:
 
 if "--inserts" in sys.argv[1:]:
 	print "CREATE INDEX i_words_word ON words (word);"
+	
+	print "CREATE TABLE ft_patterns (pattern TEXT, id PRIMARY_KEY);"
+	print "CREATE TABLE ft_matches (pattern_id NUMERIC, word_id NUMERIC);"
+	pattern_id = 0
+	for ft_pattern in ft_patterns:
+		# ignore very frequent patterns
+		if len( ft_patterns[ft_pattern] ) > 1000:
+			continue
+		print "INSERT INTO ft_patterns (pattern,id) values ('"+ft_pattern+"',"+str(pattern_id)+");"
+		for word_id in ft_patterns[ft_pattern]:
+			print "INSERT INTO ft_matches (pattern_id,word_id) values ("+str(pattern_id)+","+str(word_id)+");"
+		pattern_id += 1
+	print "CREATE INDEX i_ft_patterns_pattern ON ft_patterns (pattern);"
+	print "CREATE INDEX i_ft_matches_pattern_id ON ft_matches (pattern_id);"
+	print "CREATE INDEX i_ft_matches_word_id ON ft_matches (word_id);"
 	print "COMMIT;"
 
