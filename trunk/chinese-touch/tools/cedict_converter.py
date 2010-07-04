820cd7db8cb9e537b6f4cc6d7b3b7f97ab3d146a
syslock <syslock@fd0ee07c-f0f7-11de-b2c3-d77de7315633>
2010-07-04 18:17:41 +0000

    - improved cc-cedict converter (try to pack multiple english definitions into one entry, convert inline pinyin within translation field, unlimited hits per keyword, try to remove most chinese characters from index) - replaced full ct-dictionary, containing both writings with one for each (most users only need one, both can be combined, cross-references still there)

--- trunk/chinese-touch/tools/cedict_converter.py
+++ trunk/chinese-touch/tools/cedict_converter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import sys, re, time
+import sys, re, time, copy
 
 first_mapping = {
 	"a1" : "ā",
@@ -19,6 +19,7 @@ first_mapping = {
 	"o3" : "ǒ",
 	"o4" : "ò",
 	"o5" : "o",
+	"r5" : "r",
 }
 
 second_mapping = {
@@ -37,6 +38,11 @@ second_mapping = {
 	"ü3" : "ǚ",
 	"ü4" : "ǜ",
 	"ü5" : "ü",
+	"u:1" : "ǖ",
+	"u:2" : "ǘ",
+	"u:3" : "ǚ",
+	"u:4" : "ǜ",
+	"u:5" : "ü",
 }
 
 untone_mapping = {}
@@ -51,11 +57,18 @@ def translate_pinyin( raw_pinyin ):
 	syllables = raw_pinyin.split(" ")
 	new_syllables = []
 	for _syl in syllables:
-		_syl = _syl.lower()
+		lowercased = False
+		if( _syl != _syl.lower() ):
+			_syl = _syl.lower()
+			lowercased = True
 		try:
 			syl, tone = _syl[:-1].decode("utf-8"), int(_syl[-1:])
 		except:
 			new_syllables.append( _syl )
+			continue
+		
+		if syl=="r" and tone==5 and len(new_syllables):
+			new_syllables[-1] += syl
 			continue
 		
 		new_syl = ""
@@ -82,6 +95,9 @@ def translate_pinyin( raw_pinyin ):
 			new_syl = new_syl[::-1]
 		if new_syl==syl and tone!=5:
 			new_syl = _syl
+		
+		if lowercased:
+			new_syl = new_syl[0].upper()+new_syl[1:]
 		
 		new_syllables.append( new_syl )
 		#print _syl + " -> " + new_syl
@@ -126,6 +142,20 @@ def extended_lower( s ):
 		if extended_lower_map.has_key(c):
 			c = extended_lower_map[c]
 		result += c
+	return result
+
+# UNIHAN Range:
+#   CJK Unified Ideographs Extension A: Range: 3400-4DBF
+#   CJK Unified Ideographs: Range: 4E00-9FAF
+# Dictionary Radicals:
+#   Kangxi Radicals: Range 2F00-2FDF 
+def filter_pattern( s ):
+	result = ""
+	for c in s.decode("utf-8"):
+		if 0x3400<=ord(c)<=0x4DBF or 0x4E00<=ord(c)<=0x9FAF:
+			continue
+		else:
+			result += c.encode("utf-8")
 	return result
 
 
@@ -180,6 +210,9 @@ for line in sys.stdin:
 	words = { "trad" : result[0], "simp" : result[1] }
 	pronunciation = translate_pinyin( result[2] )
 	raw_definition = result[3]
+	inline_pinyin = re.findall( "\[([^\]]*)\]", raw_definition )
+	for pinyin in inline_pinyin:
+		raw_definition = raw_definition.replace( pinyin, translate_pinyin(pinyin) )
 	fixed_definition = ""
 	parenthesis_level=0
 	# try to fix slashes in parenthesis:
@@ -199,41 +232,68 @@ for line in sys.stdin:
 	#                      transl.    comment
 	results = re.findall( "(.*?) *(?:\(([^\(]*)\))? *[/]", fixed_definition )
 	test=False
-	for duplicate_id in xrange(len(results)):
-		test=True
-		for variant in "trad", "simp":
-			if variant=="simp" and words["simp"]==words["trad"]:
-				break
-			word = words[variant]
-			result = results[ duplicate_id ]
-			definition = result[0]
-			comment = result[1]
-			if words["simp"] != words["trad"]:
-				if len(comment):
-					comment += "; "
-				if variant=="trad":
-					comment += "simpl: " + words["simp"]
-				elif variant=="simp":
-					comment += "trad: " + words["trad"]
+	variants = []
+	if "--simplified" in sys.argv or "-s" in sys.argv:
+		variants.append( "simp" )
+	if "--traditional" in sys.argv or "-t" in sys.argv:
+		variants.append( "trad" )
+	if len(variants)==0:
+		variants = [ "trad", "simp" ]
+	duplicate_id = 0
+	for variant in variants:
+		duplicate_id = 0
+		definition = ""
+		comment = ""
+		if words["simp"] != words["trad"]:
+			if variant=="trad":
+				comment += "simpl: " + words["simp"]
+			elif variant=="simp":
+				comment += "trad: " + words["trad"]
+		if ("simp" in variants) and ("trad" in variants) \
+			and variant=="simp" and words["simp"]==words["trad"]:
+				break # only one entry if trad==simpl!
+		def write_row( definition, comment, parent_locals ):
+			global id, duplicate_id
+			parent_locals = copy.copy( parent_locals )
+			parent_locals["definition"] = definition
+			parent_locals["comment"] = comment
 			insert = "insert into words ("+",".join(col_names)+") values ("
 			ft_pattern_list = []
 			for i in xrange(len(col_names)):
 				if( i ): insert+=","
-				value = ("%("+col_names[i]+")s") % locals()
+				value = ("%("+col_names[i]+")s") % parent_locals
 				if( col_names[i] in ft_col_names ):
-					ft_pattern_list += re.split( "[ ,;:.\!\?\-/\(\)\[\]\{\}\<\>0-9'\"]", value )
+					ft_pattern_list += re.split( "[ ,;:.\!\?\-/\(\)\[\]\{\}\<\>0-9'\"\|]", value )
 				value = value.replace( "'", "''" ).strip()
 				insert += "'"+value+"'"
 			insert += ");"
 			print insert
 			for ft_pattern in ft_pattern_list:
 				ft_pattern = extended_lower( ft_pattern )
+				ft_pattern = filter_pattern( ft_pattern )
 				# do not generate unnecessary self and bogus references:
 				if ft_pattern not in words.values() + [""]:
 					if ft_patterns.has_key(ft_pattern):
 						ft_patterns[ft_pattern][id] = True
 					else: ft_patterns[ft_pattern] = { id : True }
 			id+=1
+			duplicate_id+=1
+		for di in xrange(len(results)):
+			test=True
+			word = words[variant]
+			result = results[ di ]
+			part_definition = result[0]
+			part_comment = result[1]
+			if len(part_comment):
+				if len(comment):
+					part_comment = part_comment+"; "+comment
+				write_row( part_definition, part_comment, locals() )
+			else:
+				if len(definition):
+					definition += "; "
+				definition += part_definition
+		if len(definition) or not duplicate_id:
+			write_row( definition, comment, locals() )
 	if( not test ):
 		print line
 		raise Exception("Parse error")
@@ -245,9 +305,9 @@ print "CREATE TABLE ft_patterns (pattern TEXT, id PRIMARY_KEY);"
 print "CREATE TABLE ft_matches (pattern_id NUMERIC, word_id NUMERIC);"
 pattern_id = 0
 for ft_pattern in ft_patterns:
-	# ignore very frequent patterns
-	if len( ft_patterns[ft_pattern] ) > 1000:
-		continue
+	## ignore very frequent patterns
+	#if len( ft_patterns[ft_pattern] ) > 1000:
+	#	continue
 	print "INSERT INTO ft_patterns (pattern,id) values ('"+ft_pattern+"',"+str(pattern_id)+");"
 	for word_id in ft_patterns[ft_pattern]:
 		print "INSERT INTO ft_matches (pattern_id,word_id) values ("+str(pattern_id)+","+str(word_id)+");"
