b72bfae415a9d422c4ecc724dd380c359f488021
syslock <syslock@fd0ee07c-f0f7-11de-b2c3-d77de7315633>
2011-04-22 14:28:40 +0000

    - support stroke order progress pages to some extent - automatically create destination dir

--- trunk/chinese-touch/tools/resize_stroke_order_images.py
+++ trunk/chinese-touch/tools/resize_stroke_order_images.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python2
 
-import os, re
+import os, re, os.path
 
 l=os.listdir(".")
+dest_dir = "stroke-order"
+if not os.path.isdir( dest_dir ):
+	os.mkdir( dest_dir )
 for f in l:
 	try:
 		char, suffix = re.findall( "(.*)-(.*).png", f )[0]
 	except:
 		print( "anormal file name: %s" % f )
 		continue
-	dest_file_name = "stroke-order/u%04x-%s.png" % ( ord(char.decode("utf-8")[0]), suffix )
+	dest_file_name = os.path.join( dest_dir, "u%04x-%s.png" % (ord(char.decode("utf-8")[0]), suffix) )
 	try:
 		os.stat( dest_file_name )
 	except OSError:
